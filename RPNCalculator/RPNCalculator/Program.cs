6a4658d8e35fffebce18139f5cc4d12320513179
Vincent Fantini <blurpy_the_rabid@hotmail.com>
2012-09-10 10:37:23 -0400

    Input Fix Attempt
    I'm attempting to fix the input interpretation process.  Right now it's
    still not working, but there are no compile time errors right now.  I'll
    continue poking at this tomorrow.

--- RPNCalculator/RPNCalculator/Program.cs
+++ RPNCalculator/RPNCalculator/Program.cs
@@ -10,87 +10,24 @@ namespace RPNCalculator {
             Console.WriteLine("Welcome to the Reverse Polish Notation Calculator Program, written by Vincent Fantini.");
             Console.WriteLine("This calculator only uses the basic mathematical operators: +, -, *, and /.");
             Console.WriteLine("To properly use this calculator you must enter the operands first, followed by the operator.");
-            Console.WriteLine("For example, to calculate '3 + 4', you would type, '34+'.  To calculate '3 + 5 * 7', you would type, '357*+'.");
+            Console.WriteLine("For example, to calculate '3 + 4', you would type, '3 4 +'.  To calculate '3 + 5 * 7', you would type, '3 5 7 * +'.");
             Console.Write("Please type in the mathematical operation that you wish to calculate: ");
             string userInput = Console.ReadLine();
+            string[] tokens = userInput.Split(' ');
+            Stack tokenStack = new Stack(); // This Stack shall contain the user's input.
 
-            Stack tokens = new Stack(); // This Stack shall contain the user's input.
-
-            for (int i = 0; i < userInput.Length; i++) {
-                tokens.Push(userInput[i].ToString());
+            foreach (string token in tokens) {
+                if (token == "+" || token == "-" || token == "*" || token == "/") {
+                    tokenStack.Pop();
+                }
+                else {
+                    tokenStack.Push(token);
+                }
             }
+            Console.WriteLine("Answer: {0}", tokenStack);
+
 
             // Now that we've assigned the user's input to a Stack, we'll now scroll through the user's input and perform calculations whenever an operator is encountered.
-            int x = 0, y = 0;
-            char op = '0';
-            int answer = 0;
-
-            while (tokens.Count > 0) {
-                op = (char)tokens.Pop();
-                y = (int)tokens.Pop() - '0';
-                x = (int)tokens.Pop() - '0';
-            }
-
-            switch (op) {
-                case '+':
-                    answer = x + y;
-                    Console.WriteLine("{0} + {1} = {2}", x, y, answer);
-                    break;
-                case '-':
-                    answer = x - y;
-                    Console.WriteLine("{0} - {1} = {2}", x, y, answer);
-                    break;
-                case '*':
-                    answer = x * y;
-                    Console.WriteLine("{0} * {1} = {2}", x, y, answer);
-                    break;
-                case '/':
-                    answer = x / y;
-                    Console.WriteLine("{0} / {1} = {2}", x, y, answer);
-                    break;
-            }
-
-            /*
-                        for (; ; ) {
-                            if (userInput == null) break;
-
-                            char[] sp = new char[] { ' ', '\t' };
-                            Stack<string> tokens = new Stack<string>(userInput.Split(sp, StringSplitOptions.RemoveEmptyEntries));
-                            if (tokens.Count == 0) continue;
-                            try {
-                                double r = evalrpn(tokens);
-                                if (tokens.Count != 0) throw new Exception();
-                                Console.WriteLine(r);
-                            }
-                            catch (Exception e) {
-                                Console.WriteLine("error");
-                                break;
-                            }
-                        }
-                    }
-
-                    private static double evalrpn(Stack<string> tokens) {
-                        string token = tokens.Pop();
-                        double x, y = 0;
-                        if (!Double.TryParse(token, out x)) {
-                            y = evalrpn(tokens); x = evalrpn(tokens);
-                        }
-                        if (token == "+") {
-                            x += y;
-                        }
-                        else if (token == "-") {
-                            x -= y;
-                        }
-                        else if (token == "*") {
-                            x *= y;
-                        }
-                        else if (token == "/") {
-                            x /= y;
-                        }
-                        else throw new Exception();
-                        return x;
-                    }
-            */
         }
     }
 }
